4287dfbd423d471f68030ab9ea95c2875670ba7a
Dorell James <galangdj@gmail.com>
2020-11-19 10:24:08 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e6458d32005879f018378e39111973b'),
+    },
+  },
+});
